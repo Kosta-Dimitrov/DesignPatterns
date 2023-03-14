5609f0538d7b53331637d96dc0fa00a0879d310a
Kosta Dimitrov <kostadimitrov19@gmail.com>
2023-03-14 15:52:07 +0200

    Added singleton design pattern

---
+++ Creational/Singleton/Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.GetInstance();
+            Singleton singleton2 = Singleton.GetInstance();
+
+            Console.WriteLine("singleton1 hashcode: " + singleton1.GetHashCode());
+            Console.WriteLine("singleton2 hashcode: " + singleton2.GetHashCode());
+        }
+    }
+}
---
+++ Creational/Singleton/Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        // private to prevent direct construction calls
+        private Singleton() { }
+
+        // single static field which is always returned when needed
+        private static Singleton instance;
+
+        //access to the singleton instance on the first try, it creates a singleton object
+        //every other try it returns the same instance
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        public void someBusinessLogic()
+        {
+            Console.WriteLine("Do work");
+        }
+    }
+}
